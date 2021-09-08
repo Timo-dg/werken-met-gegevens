4eba2b3069b07f907cf6f99ac54a524186bb3fbc
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-08 10:14:14 +0200

    deel 2 van feestlunch en speehaldag toegevoed

--- speelhal-dag.py
+++ speelhal-dag.py
@@ -4,3 +4,4 @@ VIP = 0.37
 print(toegangsticket * 3 )
 print(VIP * 9)
 print (VIP * 9 + toegangsticket * 3)
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
