7ac1ae035a6f337991f7676c54a275ea4e82dcfe
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-08 09:11:34 +0200

    Feestlunch afgerond

---
+++ Feestlucnh.py
@@ -0,0 +1,9 @@
+croissantje = 0.39
+stokbrood = 2.78
+kortingsbon = -0.50
+
+
+print (croissantje * 17 )
+print (stokbrood * 2)
+print (kortingsbon * 3)
+print (croissantje * 17 + stokbrood * 2 + kortingsbon *3)
